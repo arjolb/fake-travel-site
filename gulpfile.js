bea4f60be2d6c207d2f748a7a140815321ef7fc4
Arjol <arjolbega1996.com>
2019-01-15 12:41:31 +0100

    Completed large hero and page section

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,48 @@
 var gulp = require('gulp'),
 watch = require('gulp-watch'),
+browserSync = require('browser-sync').create()
 postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
-cssImport=require('postcss-import'),
-browserSync=require('browser-sync');
+cssImport = require('postcss-import'),
+mixins=require('postcss-mixins');
 
-gulp.task('default', function() {
-  console.log("Hooray - you created a Gulp task.");
-});
 
-gulp.task('html', function() {
-  console.log("Imagine something useful being done to your HTML here.");
-});
-
+/* Styles tasks */
 gulp.task('styles', function() {
   return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([cssImport,cssvars, nested, autoprefixer]))
-    .pipe(gulp.dest('app/css'));
+  .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
+  .on('error',function (e) {  
+        console.log(e.toString());
+    })
+    .pipe(gulp.dest('./app/css'));
 });
 
+gulp.task('cssInject', ['styles'], function() {
+    return gulp.src('./app/css/styles.css')
+      .pipe(browserSync.stream());
+  });
+/*Styles task* end/
+
+/*Watch tasks*/
 gulp.task('watch', function() {
 
-  browserSync.init({
-   server:{
-    baseDir:  "app"
-   }
-  })
-
-  watch('./app/index.html', function() {
-    gulp.start('html');
+    browserSync.init({
+      notify: false,
+      server: {
+        baseDir: "app"
+      }
+    });
+  
+    watch('./app/index.html', function() {
+      browserSync.reload();
+    });
+  
+    watch('./app/assets/styles/**/*.css', function() {
+      gulp.start('cssInject');
+    });
+  
   });
 
-  watch('./app/assets/styles/**/*.css', function() {
-    gulp.start('styles');
-  });
-
-});
+/*Watch tasks end*/
